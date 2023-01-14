ffda8f7104fd47129da52f63033575d856b07438
James <uptownhr@gmail.com>
2023-01-13 16:20:30 -0800

    added all content

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'James Lee - Software Engineer';
+export const SITE_DESCRIPTION = 'Software Engineer, a husband, and a father of two kids. I enjoy tackling issues related to SMBs and enabling entrepreneurs. My mission is to remove the friction of one becoming an entrepreneur and increase the amount of Small Business in the world.';
