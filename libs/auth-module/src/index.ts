1034463a6c682241f71f9f4a913cda6a26629272
uptown <uptownhr@gmail.com>
2023-01-30 23:13:52 -0800

    exporting supertokens service

--- libs/auth-module/src/index.ts
+++ libs/auth-module/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/auth.module';
 export * from './lib/super-tokens-exception.filter';
 export * from './lib/auth.middleware';
 export * from './lib/auth.guard';
+export * from './lib/super-tokens.service';
