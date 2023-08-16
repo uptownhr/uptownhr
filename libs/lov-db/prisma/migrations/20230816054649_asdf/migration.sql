ef587a7de89e754b9ce5bebc8b1165158720e6cf
James <uptownhr@gmail.com>
2023-08-15 22:50:14 -0700

    added some tools to generate and run prisma commands

---
+++ libs/lov-db/prisma/migrations/20230816054649_asdf/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Page" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "slug" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
