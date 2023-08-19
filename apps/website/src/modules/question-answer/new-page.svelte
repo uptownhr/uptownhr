cb42c4c8652fbe630ea51f9736e0092fd33a4320
James <uptownhr@gmail.com>
2023-08-19 10:15:02 -0700

    feat: prototype of qna pages

---
+++ apps/website/src/modules/question-answer/new-page.svelte
@@ -0,0 +1,27 @@
+<script>
+  let form = {
+    question: null
+  }
+
+  $: questions = []
+
+  function addQuestion() {
+    questions = [...questions, form.question]
+    form.question = null
+  }
+</script>
+
+<div>
+  <h1 contenteditable="true">What is the Q & A Topic? - <button>Save</button></h1>
+
+  <form on:submit|preventDefault={addQuestion}>
+    <input type="text" placeholder="Enter a question" bind:value={form.question} />
+    <button type="submit">Add</button>
+  </form>
+
+  <ul>
+    {#each questions as question}
+    <li>{question}</li>
+    {/each}
+  </ul>
+</div>
