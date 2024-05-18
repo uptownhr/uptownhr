3d97cc45e87c4a95fdd98701cd074eabd178cd0a
James <uptownhr@gmail.com>
2024-05-17 18:14:54 -0700

    fix: excluding draft from sitemap

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -2,10 +2,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://uptownhr.com',
-  integrations: [mdx(), sitemap(), svelte()]
+  integrations: [
+    mdx(),
+    sitemap({
+      filter(page) {
+        console.log('page', page);
+        return !page.includes('draft');
+      },
+    }),
+    svelte(),
+  ],
 });
